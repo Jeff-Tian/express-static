9c8550bf7d98008c64f3fd0fb4b639fdc8bd7c22
Jeff <Jeff-Tian@users.noreply.github.com>
2016-06-12 11:49:51 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+
+var staticFolder = __dirname + '/public';
+
+try{
+  var stats = fs.lstatSync(__dirname + '/dist');
+  if(stats.isDirectory()){
+    staticFolder = __dirname + '/dist';
+  }
+} catch (ex){
+  console.error(ex);
+}
+
+var staticSetting = {
+    etag: true,
+    lastModified: true,
+    maxAge: 1000 * 3600 * 24 * 30,
+    setHeaders: function (res, path) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+    }
+};
+
+app.use(express.static(staticFolder, staticSetting));
+app.use('/locales', express.static(__dirname + '/locales', staticSetting));
